5e917f00e48a2a594423101b3a93f2c4f21d893b
Raven <ravenkoehler@gmail.com>
2021-09-13 16:35:43 -0400

    commit tree 8d23109236c61aec12aa02b7adc89148c0bdcc82

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function mapToNegativize(numbers) {
+    const negNums = []
+    numbers.forEach(num => negNums.push(num / -1))
+    return negNums;
+}
+
+function mapToNoChange(numbers) {
+    const nums = [];
+    numbers.forEach(num => nums.push(num));
+    return nums;
+}
+
+function mapToDouble(numbers) {
+    const nums = [];
+    numbers.forEach(num => nums.push(num * 2));
+    return nums;
+}
+
+function mapToSquare(numbers) {
+    const nums = [];
+    numbers.forEach(num => nums.push(num ** 2));
+    return nums;
+}
+
+function reduceToTotal(numbers, start = 0) {
+    let total = start;
+    numbers.forEach(num => total += num);
+    return total;
+}
+
+function reduceToAllTrue(booleans) {
+    let boolean = true;
+    booleans.forEach(function(bool){if(!bool){boolean = false; return boolean}});
+    return boolean;
+}
+
+function reduceToAnyTrue(booleans) {
+    let boolean = false;
+    booleans.forEach(function(bool){if(bool){boolean = true; return boolean}});
+    return boolean;
+}
